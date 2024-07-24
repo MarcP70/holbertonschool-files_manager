d048bd4e5961e9d3308f1597ad0541f019e2c82b
MarcP70 <marcpourias@yahoo.fr>
2024-07-24 11:26:38 +0200

    Add UserController.js to add a new user and update index.js to add a route for new user

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const app = express.Router();
 
 app.get('/status', AppController.getStatus);
 
 app.get('/stats', AppController.getStats);
+
+app.post('/users', UsersController.postNew);
 
 export default app;
