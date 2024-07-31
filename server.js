cda6e47ab4e6e851b8d2827bfdd449b9bbbd30c4
MarcP70 <marcpourias@yahoo.fr>
2024-07-31 19:07:50 +0200

    Update server.js for ESLint

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import express from 'express';
+
+// Importez et configurez Bull Board
+import { createBullBoard } from 'bull-board';
+import { BullAdapter } from 'bull-board/bullAdapter';
 import router from './routes';
+import fileQueue from './worker';
+
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -11,19 +17,13 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json());
 
-app.use('/', router);
-
-
-// Importez et configurez Bull Board
-import { createBullBoard } from 'bull-board';
-import { BullAdapter } from 'bull-board/bullAdapter';
-import fileQueue from './worker'; // Importez votre queue
+app.use('/', router); // Importez votre queue
 const { router: bullBoardRouter } = createBullBoard([
   new BullAdapter(fileQueue),
 ]);
 app.use('/admin/queues', bullBoardRouter);
 
-//Lancement du serveur
+// Lancement du serveur
 app.listen(port, () => {
   console.log('Server running on port', port);
 });
