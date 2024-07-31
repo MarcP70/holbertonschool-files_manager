bea8397514c93c02f330af7aec6f57321eedc83a
MarcP70 <marcpourias@yahoo.fr>
2024-07-31 18:49:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import router from './routes';
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Augmenter la limite de taille des fichiers d'upload
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json());
 
 app.use('/', router);
 
+
+// Importez et configurez Bull Board
+import { createBullBoard } from 'bull-board';
+import { BullAdapter } from 'bull-board/bullAdapter';
+import fileQueue from './worker'; // Importez votre queue
+const { router: bullBoardRouter } = createBullBoard([
+  new BullAdapter(fileQueue),
+]);
+app.use('/admin/queues', bullBoardRouter);
+
+//Lancement du serveur
 app.listen(port, () => {
   console.log('Server running on port', port);
 });
